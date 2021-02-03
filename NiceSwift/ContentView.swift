76ae9519aef12250cfc178dfd3eca4528e074f12
zack <shiqizhenyes@gmail.com>
2021-02-03 17:24:40 +0800

    Initial Commit

---
+++ NiceSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NiceSwift
+//
+//  Created by zack on 2021/2/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NiceSwift/NiceSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceSwiftApp.swift
+//  NiceSwift
+//
+//  Created by zack on 2021/2/3.
+//
+
+import SwiftUI
+
+@main
+struct NiceSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
